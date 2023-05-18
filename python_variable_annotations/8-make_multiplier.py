4fd881eab9c8a967c4aeba6494283e86f9caf087
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-17 19:34:25 -0500

    Function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier.

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module containing an annotated function make_multiplier that takes a
+float multiplier as argument and returns a function that multiplies a
+float by multiplier.
+"""
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """
+    Function make_multiplier that takes a float multiplier as argument and
+    returns a function that multiplies a float by multiplier.
+    """
+    return lambda multiplier: multiplier * multiplier
