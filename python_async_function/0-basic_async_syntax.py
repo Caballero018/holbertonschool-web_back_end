7044327dc775856d5901ecf8c6daaa3cc6f452db
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-23 22:27:23 -0500

    Changes

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Asynchronous coroutine that takes in an integer argument (max_delay, with a
+default value of 10) named wait_random that waits for a random delay between
+0 and max_delay (included and float value) seconds and eventually returns it.
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10):
+    random_number = random.randint(0, max_delay)
+    
