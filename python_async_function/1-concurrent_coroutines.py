12cfc9b8f1b44e2b376658b0a41628e54c23fa93
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-24 13:18:33 -0500

    Async routine

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Async routine called wait_n that takes in 2 int arguments (in this order):
+n and max_delay. You will spawn wait_random n times with the specified
+max_delay.
+"""
+import asyncio
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int): # -> typing.List[float]:
+    all_delays = []
+
+    a = await asyncio.gather(wait_random)    
+    return a
