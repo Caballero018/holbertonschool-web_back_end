4712c32ad6c21510a1627d0ea999dd635d84f138
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-25 16:01:59 -0500

    a

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""""""
+""" d """
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int):
+    """ d """
     return asyncio.create_task(wait_random(max_delay))
