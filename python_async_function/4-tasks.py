d319f6cda8cd5ced39f9b5296b8e99b7e84ebf03
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-25 16:24:35 -0500

    f

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Async routine called wait_n that takes in 2 int arguments (in this order):
+n and max_delay. You will spawn wait_random n times with the specified
+max_delay.
+"""
+import asyncio
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """ Async routine """
+    all_delays = []
+    gather = await asyncio.gather(wait_random(max_delay))
+    for _ in range(n):
+        all_delays.append(task_wait_random(gather[0]))
+    return all_delays
