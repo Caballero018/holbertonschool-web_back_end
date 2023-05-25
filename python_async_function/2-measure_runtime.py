c0bdb343399e327200e5174218d14e77cf71c0ad
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-25 15:22:27 -0500

    Function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay), and returns total_time / n.

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-h
+Module that have a function with integers n and max_delay as arguments
+that measures the total execution time for wait_n(n, max_delay), and
+returns total_time / n.
 """
 import asyncio
 import time
@@ -8,7 +10,11 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ h """
+    """
+    Function with integers n and max_delay as arguments that measures the
+    total    execution time for wait_n(n, max_delay), and returns total_time
+    / n.
+    """
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - start
