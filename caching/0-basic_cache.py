1eefff70c955d82fe6c2deb21154c3ffbb9a80d3
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-26 12:01:18 -0500

    Class BasicCache that inherits from BaseCaching and is a caching system

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module that have a class BasicCache that inherits from BaseCaching and is
+caching system.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Class BasicCache that inherits from BaseCaching and is a caching
+    system
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
