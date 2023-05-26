bc26c2dbd924f6adc20650d3da21c6bb591c4a2a
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-26 10:54:30 -0500

    Coroutine called async_comprehension that takes no arguments.

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+The coroutine will collect 10 random numbers using an async comprehensing over
+async_generator, then return the 10 random numbers.
+"""
+import asyncio
+import typing
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> typing.List[float]:
+    """ Coroutine called async_comprehension that takes no arguments. """
+    return [delay async for delay in async_generator()]
