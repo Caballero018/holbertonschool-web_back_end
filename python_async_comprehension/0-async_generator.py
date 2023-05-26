f0ea1b2c3da15574d535314d8e579b3ad09aef76
Caballero018 <alejoxcaballero18@gmail.com>
2023-05-25 22:01:07 -0500

    Coroutine called async_generator that takes no arguments.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 """
-
-"""
-import random
-import asyncio
-"""
 The coroutine will loop 10 times, each time asynchronously
 wait 1 second, then yield a random number between 0 and 10
 """
+import random
+import asyncio
 import typing
 
 
 async def async_generator() -> typing.Generator[float, None, None]:
-    "Coroutine called async_generator that takes no arguments."
+    """Coroutine called async_generator that takes no arguments."""
     for i in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
