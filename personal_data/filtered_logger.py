d1a224d0bb48a4b061556ee47452f0b8c9c73576
holberton <holberton@holbertons-iMac.local>
2023-05-31 14:22:20 -0500

    changes

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that have a function called filter_datum that returns the log message obfuscated """
+import typing
+
+
+def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
+    """ Function called filter_datum that returns the log message obfuscated """
+    pass
