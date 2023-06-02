cd9607b043fb062652de152e2d30a71fbdc2b8ec
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-01 19:27:32 -0500

    Function called filter_datum that returns the log message obfuscated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-""" Module that have a function called filter_datum that returns the log message obfuscated """
-import typing
+"""
+Module that have a function called filter_datum that returns the log message
+obfuscated
+"""
+from typing import List
+import re
 
 
-def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
-    """ Function called filter_datum that returns the log message obfuscated """
-    pass
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Function called filter_datum that returns the log message obfuscated
+    """
+    for field in fields:
+        message = re.sub("(?<={:s}=)(.*?)(?={:s})".format(field, separator),
+                         redaction, message)
+    return message
