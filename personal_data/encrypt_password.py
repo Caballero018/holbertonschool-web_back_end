6767e850f5adb8ca4d518dc3d0043ed3edf93e4b
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-04 23:42:56 -0500

    :white_check_mark:

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -15,3 +15,12 @@ def hash_password(password: str) -> bytes:
     hash = bcrypt.hashpw(bytes, salt)
 
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check valid password
+    """
+    if bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password):
+        return True
+    return False
