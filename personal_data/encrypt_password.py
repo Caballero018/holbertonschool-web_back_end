120a26b96128db41710b6e9bfbfeade2cc894b1a
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-04 23:39:07 -0500

    Changes

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Function that expects one string argument name password and returns
+    a salted, hashed password, which is a byte string.
+    """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+
+    return hash
