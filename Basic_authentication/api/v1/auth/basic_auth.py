3514bae34ef1bcb58eae87652fff67e238bb969a
holberton <holberton@holbertons-iMac.local>
2023-06-07 16:50:25 -0500

    Changes

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,6 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Doc """
-    pass
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if not base64_authorization_header or type(base64_authorization_header) is not str:
+            return None
