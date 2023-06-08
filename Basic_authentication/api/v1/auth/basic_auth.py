306a49a8a3c538a176b84a67f3ba2c820c7b1bb4
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-08 11:46:54 -0500

    :rocket:

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """
-Doc
+Module has a class llamed BasicAuth
 """
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ Doc """
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        if not base64_authorization_header or type(base64_authorization_header) is not str:
+    """ Class BasicAuth that inherits from Auth """
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """
+        Method that returns the Base64 part of the Authorization header for a
+        Basic Authentication
+        """
+        if not authorization_header or type(authorization_header) is not str\
+                or "Basic " not in authorization_header[:6]:
             return None
+        return authorization_header[6:]
