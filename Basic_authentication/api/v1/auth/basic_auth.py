42528d226c71ec221a0d951185d10569f9118eee
holberton <holberton@holbertons-iMac.local>
2023-06-08 14:39:37 -0500

    :rocket:

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Module has a class llamed BasicAuth
 """
 from api.v1.auth.auth import Auth
+from codecs import decode
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +19,18 @@ class BasicAuth(Auth):
                 or "Basic " not in authorization_header[:6]:
             return None
         return authorization_header[6:]
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str)\
+            -> str:
+        """
+        Method that returns the decoded value of a Base64 string
+        'base64_authorization_header'
+        """
+        try:
+            if not base64_authorization_header or type(base64_authorization_header) is not str:
+                return None
+            base = base64_authorization_header.encode()
+            base = base64.b64decode(base)
+            return decode(base, 'utf-8', 'strict')
+        except base64.binascii.Error:
+            return None
