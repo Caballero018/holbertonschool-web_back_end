5930ae697ff9edcffb317f4eab4718df30f186cf
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-11 15:16:15 -0500

    Update the @app.before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,12 +59,13 @@ def before_request():
         ]
     if not auth or not auth.require_auth(request.path, paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
     if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
+        abort(401)
 
     request.current_user = auth.current_user(request)
 
