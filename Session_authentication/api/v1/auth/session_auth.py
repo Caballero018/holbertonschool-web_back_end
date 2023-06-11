dcfb759ef02a77f9e0cbc3ed4d7723e36a714772
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-11 14:11:23 -0500

    Methods (create_session and user_id_for_session_id) for storing and retrieving a link between a User ID and a Session ID.

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,8 +8,10 @@ import uuid
 
 class SessionAuth(Auth):
     """
-    Doc
+    Methods (create_session and user_id_for_session_id) for storing and
+    retrieving a link between a User ID and a Session ID.
     """
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -21,3 +23,12 @@ class SessionAuth(Auth):
         id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Method that returns a User ID based on a Session ID.
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        user_value = SessionAuth.user_id_by_session_id.get(session_id)
+        return user_value
