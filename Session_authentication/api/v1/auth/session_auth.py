22f6efb372a29903be89044d77599a7fb6e9f14e
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-11 20:44:33 -0500

    Session Auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -54,7 +54,7 @@ class SessionAuth(Auth):
             return False
 
         session_id = super().session_cookie(request)
-        if not session_id:
+        if session_id is None:
             return False
 
         user_id = self.user_id_for_session_id(session_id)
