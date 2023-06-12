9d71f66adf268c93bb4cd76014ac37b12347ba42
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-11 20:27:53 -0500

    Session Auth

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -36,7 +36,7 @@ def login():
     return response
 
 
-@app_views.route('/api/v1/auth_session/logout', methods=['DELETE'],
+@app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
 def logout():
     """
