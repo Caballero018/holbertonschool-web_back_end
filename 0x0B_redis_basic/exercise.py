abe7aea62f60ddeb5a96e75803cf6b512c7ebc00
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-28 14:11:12 -0500

    Cache data that is set in the object of redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Modulo that has a module a class called Cache
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
+        """Method store
+
+        Keyword arguments:
+        data -- Data that is set in the dictionary
+        Return: return_description
+        """
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
