ba065d9abb39aeeffec7055d007930b93250a667
holberton <holberton@holbertons-iMac.local>
2023-06-28 15:22:11 -0500

    Update

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -22,3 +22,9 @@ class Cache():
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+    
+    def get(self, key, fn):
+        if self._redis.get(key) == None:
+            return None
+        value = self._redis.get(key)
+        return value
