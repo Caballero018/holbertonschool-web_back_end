9b0b0c5d67f1ebe2b8b84d44bdd2b9592898926e
Caballero018 <alejoxcaballero18@gmail.com>
2023-06-30 16:59:36 -0500

    Update

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,72 +1,79 @@
 #!/usr/bin/env python3
-"""
-Modulo that has a module a class called Cache
-"""
-import redis
-import uuid
-from typing import Union, Optional, Callable
+"""Redis excercise file"""
+
 from functools import wraps
+import redis
+from typing import Callable, Optional, Union
+import uuid
 
 
 def count_calls(method: Callable) -> Callable:
-    """Decorator count_calls
-
-    Keyword arguments:
-    method -- single method Callable argument
-    Return: Callable
-    """
+    """Calls counter for Cache methods"""
     @wraps(method)
     def wrapper(self, *args):
+        """Wrapped function"""
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args)
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Call logger for Cache methods"""
+    @wraps(method)
+    def wrapper(self, *args):
+        """Wrapped function"""
+        input_key = "{:s}:inputs".format(method.__qualname__)
+        output_key = "{:s}:outputs".format(method.__qualname__)
+        output = method(self, *args)
+        self._redis.rpush(input_key, str(args))
+        self._redis.rpush(output_key, output)
+        return output
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Statistics method for Cache methods"""
+    redis_engine = redis.Redis()
+    qname = method.__qualname__
+    input_key = "{:s}:inputs".format(qname)
+    output_key = "{:s}:outputs".format(qname)
+    input_list = redis_engine.lrange(input_key, 0, -1)
+    output_list = redis_engine.lrange(output_key, 0, -1)
+    print(f"{qname} was called {int(redis_engine.get(qname))} times:")
+    for input_, output_ in zip(input_list, output_list):
+        if input_:
+            input_ = input_.decode('utf-8')
+        if output_:
+            output_ = output_.decode('utf-8')
+        print("{}(*{}) -> {}".format(qname, input_, output_))
+
+
 class Cache():
+    """Cache class using redis"""
+
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Method store
+        """Stores data to a random key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
 
-        Keyword arguments:
-        data -- Data that is set in the object
-        Return: return_description
-        """
-        random_key = str(uuid.uuid4())
-        self._redis.set(random_key, data)
-        return random_key
-
-    def get(self, key: str, fn: Optional[callable] = None):
-        """Method get
-
-        Keyword arguments:
-        key -- string argument
-        fn  -- optional Callable argument
-        Return: return_description
-        """
-        value = self._redis.get(key)
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """Gets a key from Redis"""
         if fn:
-            return fn(value)
-        return value
+            return fn(self._redis.get(key))
+        return (self._redis.get(key))
 
-    def get_str(self, key: str) -> str:
-        """Method get_str
+    def get_str(self, key: str):
+        """Returns a str from redis"""
+        return (self.get(key, str))
 
-        Keyword arguments:
-        key -- string argument
-        Return: conversion of data to str
-        """
-        return self.get(key, str)
-
-    def get_int(self, key: int) -> int:
-        """Method get_str
-
-        Keyword arguments:
-        key -- string argument
-        Return: conversion of data to int
-        """
-        return self.get(key, int)
+    def get_int(self, key: str):
+        """Returns int from redis"""
+        return (self.get(key, int))
