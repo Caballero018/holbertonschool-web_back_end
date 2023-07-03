bc067922bd8f5a477aafd9612dc242ab1b749d0b
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-03 18:13:39 -0500

    Script that creates a table users

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table users
-CREATE TABLE users (
+CREATE TABLE users IF NOT EXISTS (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
