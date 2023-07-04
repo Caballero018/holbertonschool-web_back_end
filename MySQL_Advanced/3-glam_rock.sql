3ead013dd08bb40a72879af21ed2d727838bb7dc
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-03 21:37:45 -0500

    Script that lists all bands with Glam rock as their main style, ranked by their longevity

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- Script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, formed, split FROM metal_bands WHERE split IS NOT NULL AND split != '';
+SELECT band_name, formed, split FROM metal_bands WHERE split IS NOT NULL AND split;
