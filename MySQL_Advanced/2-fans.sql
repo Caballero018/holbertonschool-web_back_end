fc1ec8b43570046e4edb795c79df301a7c7de48b
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-03 20:20:32 -0500

    Script that ranks country origins of bands, ordered by the number of (non-unique) fans

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- Script that ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands WHERE nb_fans > 1 ORDER BY nb_fans DESC GROUP BY origin;
+SELECT origin, fans AS nb_fans FROM metal_bands;
