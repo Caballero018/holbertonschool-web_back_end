a90ada2db44d9b7470061800f8bc9cc4a72f2316
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-03 20:43:11 -0500

    Script that lists all bands with Glam rock as their main style, ranked by their longevity

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- Script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (formed - split) AS lifespan FROM metal_bands ORDER BY lifespan DESC;
+SELECT band_name, formed - split AS lifespan FROM metal_bands;
